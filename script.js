fca8325cbc884a7bf92999b19ddf2fadfeef9f7f
Kenyansa <kenyansajustine@gmail.com>
2023-07-11 22:38:47 +0300

    edit code to allow actual twitting

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ async function getQuote(url){
 getQuote(api_url)
 
 function tweet(){
-    window.open("https://twitter.com/intent/tweet?text=Hello%20world", "Tweet Window", "width=600, height=400");
+    window.open("https://twitter.com/intent/tweet?text=" + quoteElement.innerHTML, "Tweet Window", "width=600, height=400");
 }
