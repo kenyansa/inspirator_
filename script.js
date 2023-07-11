7d0579a6aa1ce081fe17de4a5e79893493c5c3c3
Kenyansa <kenyansajustine@gmail.com>
2023-07-11 22:04:37 +0300

    correct typo in html and get api fetch working

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const api_url = "https://api.api-ninjas.com/v1/quotes?category=health"
 const apiKey = 'soP2DeFDH0h3N+n7RWfAiA==7bCg5koQKLpC0Av5'
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
 
 async function getQuote(url){
     const res = await fetch(url, {
@@ -9,5 +11,9 @@ async function getQuote(url){
     });
     const data = await res.json();
     console.log(data);
+    //set innerHTML of the quote element
+    quoteElement.innerHTML = data[0].quote
+    // Set theinnerHTML of the author element
+    authorElement.innerHTML = data[0].author;
 }
 getQuote(api_url)
